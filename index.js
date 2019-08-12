6cd004c80d8f24b18269d07a9f29534e8b390ce9
koppelaharsha <koppelaharsha@gmail.com>
2019-08-12 23:01:11 +0530

    adjusted logger and routes handler

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ const sessions = require('./util/sessions');
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const helmet = require('helmet');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
+const logger = require('./util/logger');
 // const cors = require('cors');
 
 const app=express();
 
-app.use(morgan(
-    ':req[x-forwarded-for] - [:date[web]] ":method :url HTTP/:http-version" :status :res[content-length] ":req[x-requested-with]" ":referrer" ":user-agent"',
-    {stream: fs.createWriteStream(path.join(__dirname, 'data', 'access.log'), { flags: 'a' })}
-));
+app.use(logger);
 // app.use(cors());
 app.use(helmet());
 app.use(favicon('./public/images/favicon.ico'));
@@ -26,26 +21,12 @@ app.use(express.static('./public'));
 // app.use(cookieParser());
 app.use(sessions);
 app.use(csrf());
-// app.use(csrf({value:(req)=>req.cookies['csrf-token']}));
-// app.use((req,res,next)=>{ res.cookie('csrf-token',req.csrfToken(),{maxAge:86400000}); next(); });
 app.use(flash());
 app.set('view engine','ejs');
 app.set('views','views');
 
-const authRoutes=require('./routes/auth');
-const userRoutes=require('./routes/user');
-// const facultyRoutes=require('./routes/faculty');
-const adminRoutes=require('./routes/admin');
-const mainRoutes=require('./routes/main');
-const donateRoutes=require('./routes/donate');
-
-app.get('/hello',(req,res)=>{res.send('Hello')});
-app.use(authRoutes);
-app.use('/u',userRoutes);
-// app.use('/f',facultyRoutes);
-app.use('/admin',adminRoutes);
-app.use(donateRoutes);
-app.use(mainRoutes);
+const routeHandler = require('./routes/routes');
+app.use(routeHandler);
 
 // const User=require('./models/Users');
 // const Blog=require('./models/Blogs');
---
+++ util/logger.js
@@ -0,0 +1,9 @@
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = morgan(
+    ':req[x-forwarded-for] - [:date[web]] ":method :url HTTP/:http-version" :status :res[content-length] ":req[x-requested-with]" ":referrer" ":user-agent"',
+    {stream: fs.createWriteStream(path.join(__dirname, '..', 'data', 'access.log'), { flags: 'a' })}
+);
+
