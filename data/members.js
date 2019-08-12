0824db823f0dd87e7c5ae5fe1d76550e38c52f61
koppelaharsha <koppelaharsha@gmail.com>
2019-08-12 17:31:15 +0530

    updated members data

--- data/members.js
+++ data/members.js
@@ -3,25 +3,25 @@ module.exports = [
     {
         "name":"Dr. Rikmantra Basu",
         "position":"Branch Counsellor",
-        "phone":"",
+        "phone":"+911133861173",
         "email":"rikmantrabasu@nitdelhi.ac.in",
         "image":"rkbasu.jpg"
     },{
         "name":"Anuveksh Taneja",
         "position":"Chairperson",
-        "phone":"+919876543210",
+        "phone":"+919871739461",
         "email":"161220007@nitdelhi.ac.in",
         "image":"anuveksh.jpg"
     },{
         "name":"Ankit Pal",
         "position":"Vice Chairperson",
-        "phone":"+919876543210",
+        "phone":"+919315540368",
         "email":"161220005@nitdelhi.ac.in",
         "image":"ankit.jpg"
     },{
         "name":"Tiyas Gosh",
         "position":"General Secretary",
-        "phone":"+919876543210",
+        "phone":"+919205386802",
         "email":"161220038@nitdelhi.ac.in",
         "image":"tiyas.jpg"
     },{
@@ -29,60 +29,60 @@ module.exports = [
         "position":"Joint Secretary",
         "phone":"+919971362432",
         "email":"161210023@nitdelhi.ac.in",
-        "image":""
+        "image":"jaspreet.jpg"
     },{
         "name":"Gopesh Singh",
         "position":"Treasurer",
-        "phone":"+919876543210",
+        "phone":"+919643668301",
         "email":"161220012@nitdelhi.ac.in",
         "image":"gopesh.jpg"
     },{
         "name":"Harsha Vardhan Reddy",
         "position":"Webmaster",
-        "phone":"+919876543210",
+        "phone":"+917981244027",
         "email":"161220015@nitdelhi.ac.in",
         "image":"harsha.jpg"
     },{
         "name":"Vardhan Korada",
         "position":"Technical Head",
-        "phone":"+919876543210",
+        "phone":"+917995968475",
         "email":"161220016@nitdelhi.ac.in",
         "image":"vardhan.jpg"
     },{
         "name":"Kush Yadav",
         "position":"App Developer",
-        "phone":"+919876543210",
+        "phone":"+918376882818",
         "email":"161220019@nitdelhi.ac.in",
         "image":"kush.jpg"
     },{
         "name":"Aman Kumar Singh",
         "position":"Design lead",
-        "phone":"",
-        "email":"",
-        "image":""
+        "phone":"+919467748694",
+        "email":"161220002@nitdelhi.ac.in",
+        "image":"aman.jpg"
     },{
         "name":"Revti Raman",
         "position":"Publicity Head",
-        "phone":"+919876543210",
+        "phone":"+917065237165",
         "email":"161220036@nitdelhi.ac.in",
         "image":"revti.jpg"
     },{
         "name":"Atish Sri Kumar",
         "position":"Marketing Head",
-        "phone":"+919876543210",
+        "phone":"+918826762262",
         "email":"161220008@nitdelhi.ac.in",
         "image":"atish.jpg"
     },{
         "name":"Subasish Kar",
         "position":"Planning Head",
-        "phone":"+919876543210",
+        "phone":"+919707960561",
         "email":"161220037@nitdelhi.ac.in",
         "image":"subasish.jpg"
     },{
         "name":"Amit Kumar",
         "position":"Logistics head",
-        "phone":"",
-        "email":"",
-        "image":""
+        "phone":"+918750152844",
+        "email":"161220003@nitdelhi.ac.in",
+        "image":"amit.jpg"
     }
 ]
